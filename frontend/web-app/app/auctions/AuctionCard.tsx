7adb6dbae1923a8995c092aa06d65c30e63b5039
nhan.trinh <hongnhan50@gmail.com>
2024-02-03 12:19:44 +0700

    Adjusting AuctionCard.tsx

--- frontend/web-app/app/auctions/AuctionCard.tsx
+++ frontend/web-app/app/auctions/AuctionCard.tsx
@@ -4,11 +4,26 @@ import Image from "next/image";
 type Props = {
     auction: string
 }
-export default function AuctionCard({auction}: Props){
+export default function AuctionCard({auction}: Props) {
     return (
         <a href='#'>
-            <div className='w-full bg-gray-200 aspect-video rounded-lg overflow-hidden'>
-                <Image src={auction.imageUrl} alt='{auction.}' width={100} height={50} className='object-cover'/>
+            <div className='w-full bg-gray-200 aspect-w-16 aspect-h-10 rounded-lg overflow-hidden'>
+                <div>
+                    <Image src={auction.imageUrl}
+                           alt='image'
+                           fill
+                           priority
+                           className='object-cover'
+                           sizes='(max-width:768px) 100vw , (max-width: 1200px) 50vw , 25vw'
+                    />
+                </div>
+
+            </div>
+            <div className='flex justify-between items-center mt-4' >
+                <h3 className='text-gray-700'>
+                    {auction.make} {auction.model}
+                </h3>
+                <p className='font-semibold text-sm'>{auction.year}</p>
             </div>
         </a>
     );
